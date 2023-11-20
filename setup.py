b42b5555c47085471b88461248553e3c8104990c
user-sspmynxdvb <pewfellowmnemonic@gmail.com>
2023-11-20 14:54:04 +0200

    v1.3.1

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="ua_alarm",
     author="user-sspmynxdvb",
-    version="1.3.0",
+    version="1.3.1",
     description="Implements api.ua_alarm.com API that returns info about Ukraine air raid alarms.",
     license="GNUv3",
     url="https://github.com/user-sspmynxdvb/ua_alarm",
-    packages=["ua_alarm"],
+    packages=find_packages(),
     install_requires=[
         'aiohttp>=3.9.0', 'pydantic>=2.5.1'
     ],
